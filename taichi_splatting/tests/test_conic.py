Simplify and fix gradients for generalized gaussians, fix bug in grad test

taichi_splatting/tests/test_conic.py
@@ -8,7 +8,10 @@ from taichi_splatting.taichi_lib.f64 import (
 
 from taichi_splatting.tests.util import compare_with_grad
 
+
 import warnings
+
+from taichi_splatting.torch_ops.projection import cov_to_conic
 warnings.filterwarnings('ignore') 
 
 
@@ -20,9 +23,7 @@ def torch_conic_pdf(xy:torch.Tensor, uv:torch.Tensor, uv_conic:torch.Tensor, bet
     a, b, c = uv_conic.T
 
     inner = 0.5 * (dx**2 * a + dy**2 * c) + dx * dy * b
-
-    print(inner)
-    return torch.exp(-inner.pow(1))
+    return torch.exp(-inner.pow(beta))
 
 
 
@@ -38,10 +39,11 @@ def kernel_conic_pdf_grad(
    dp_dbeta : ti.types.ndarray(ti.f64, ndim=1)):
 
    for i in range(uv.shape[0]):
-      _, grad_uv, grad_conic, grad_beta = conic_gef_pdf_with_grad(xy[i], uv[i], uv_conic[i], beta[i])
+      p, grad_uv, grad_conic, grad_beta = conic_gef_pdf_with_grad(xy[i], uv[i], uv_conic[i], beta[i])
       dp_duv[i] = grad_uv
       dp_dconic[i] = grad_conic
       dp_dbeta[i] = grad_beta
+
 
 @ti.kernel
 def kernel_conic_pdf(
@@ -60,8 +62,9 @@ def kernel_conic_pdf(
 class ConicPdf(torch.autograd.Function):
     @staticmethod
     def forward(ctx, xy, uv, uv_conic, beta):
-        p = torch.zeros_like(xy[:, 0])
+        p = torch.zeros_like(beta)
         ctx.save_for_backward(xy, uv, uv_conic, beta, p)
+        
         kernel_conic_pdf(xy, uv, uv_conic, beta, p)
         return p
 
@@ -70,12 +73,10 @@ class ConicPdf(torch.autograd.Function):
         xy, uv, uv_conic, beta, _ = ctx.saved_tensors
         grad_uv = torch.zeros_like(uv)
         grad_conic = torch.zeros_like(uv_conic)
-        grad_beta = torch.zeros_like(uv_conic[:, 0])
+        grad_beta = torch.zeros_like(beta)
 
         kernel_conic_pdf_grad(xy, uv, uv_conic, beta, grad_uv, grad_conic, grad_beta)
-
-        grad_p = grad_p.unsqueeze(1)
-        return None, grad_uv * grad_p, grad_conic * grad_p, grad_beta * grad_p
+        return None, grad_uv * grad_p.unsqueeze(1), grad_conic * grad_p.unsqueeze(1), grad_beta * grad_p
 
 
 def random_inputs(n, device='cpu', dtype=torch.float64):
@@ -85,19 +86,28 @@ def random_inputs(n, device='cpu', dtype=torch.float64):
       dx = torch.randn(n, 2, device=device, dtype=dtype)
       uv = torch.rand(n, 2, device=device, dtype=dtype) * 100
 
-      conic = torch.randn(n, 3, device=device, dtype=dtype)
-      beta = torch.full((n, ), 1.0, dtype=dtype) #0.5 + torch.rand(n, device=device, dtype=dtype)
+      m = 10 * torch.randn(n, 2, 2, device=device, dtype=dtype)
+      cov = m @ m.transpose(-1, -2)
+
+      conic = cov_to_conic(cov)
+      beta = torch.full((n, ), 2.0, device=device, dtype=dtype) 
+      # beta = 0.5 + torch.rand(n, device=device, dtype=dtype) 
+
+      pixel = (uv + dx) 
+      params =  [pixel] + [t.contiguous().requires_grad_(True) for t in [uv, conic, beta]]
 
       # No gradient on xy as conic_pdf_with_grad doesn't provide it
-      return (uv + dx), uv.requires_grad_(True), conic.exp().requires_grad_(True), beta.requires_grad_(True)
+      return params
     return f
 
 def test_conic():
+  torch.manual_seed(0)
   compare_with_grad("conic_pdf", ["xy", "uv", "uv_conic", "beta"], "p", 
         ConicPdf.apply, torch_conic_pdf, random_inputs(10), iters=100)
 
 def test_conic_gradcheck(iters = 100, device='cpu'):
   make_inputs = random_inputs(10)
+  
 
   seeds = torch.randint(00, 10000, (iters, ), device=device)
   for seed in tqdm(seeds, desc="conic_gradcheck"):

taichi_splatting/torch_ops/projection.py
@@ -3,7 +3,8 @@ import torch
 
 from taichi_splatting.perspective import CameraParams
 from taichi_splatting.data_types import Gaussians3D
-from taichi_splatting.torch_ops.transforms import make_homog, quat_to_mat, transform33, transform44
+from taichi_splatting.torch_ops.transforms import make_homog, transform33, transform44
+import roma
 
 def inverse_sigmoid(x:torch.Tensor):
   return torch.log(x / (1 - x))
@@ -30,7 +31,7 @@ def covariance_in_camera(
     """ Construct and rotate the covariance matrix in camera space
     """
     W = T_camera_world[:3, :3]
-    R = quat_to_mat(cov_rotation)
+    R = roma.unitquat_to_rotmat(cov_rotation)
     S = torch.eye(3, device=cov_scale.device, dtype=cov_scale.dtype
                   ).unsqueeze(0) * cov_scale.unsqueeze(1)
     