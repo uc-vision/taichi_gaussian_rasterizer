90d8b6de728004bc4f5cfe7d1d5ede0764d30a75
Oliver Batchelor <saulzar@gmail.com>
2024-11-22 14:58:44 +1300

    Slighly more stable

--- taichi_splatting/examples/fit_image_gaussians.py
+++ taichi_splatting/examples/fit_image_gaussians.py
@@ -31,7 +31,7 @@ def parse_args():
   parser.add_argument('--epoch', type=int, default=20, help='base epoch size (increases with t)')
 
   parser.add_argument('--opacity_reg', type=float, default=0.0000)
-  parser.add_argument('--scale_reg', type=float, default=10.0)
+  parser.add_argument('--scale_reg', type=float, default=0.0)
 
   parser.add_argument('--debug', action='store_true')
   parser.add_argument('--show', action='store_true')
@@ -146,11 +146,7 @@ class Trainer:
     self.render_step(gaussians)
     grad =  gaussians.grad
     
-    # mean_abs_grad = grad.abs().mean(dim=0)
-    # if self.running_scales is None:
-    #   self.running_scales = mean_abs_grad
-    # else:
-    #   self.running_scales = lerp(0.99, self.running_scales, mean_abs_grad)
+
 
     scales = dict(position = 0.0002, log_scaling = 0.005, rotation = 0.0005, feature = 0.001)
 
@@ -160,8 +156,11 @@ class Trainer:
 
     return grad
 
-    # for k, v in grad.abs().items():
-    #   print(f'{k}: {v.max() * 1000.:.4f} {v.mean() * 1000.:.4f}')
+    # mean_abs_grad = grad.abs().mean(dim=0)
+    # if self.running_scales is None:
+    #   self.running_scales = mean_abs_grad
+    # else:
+    #   self.running_scales = lerp(0.9, self.running_scales, mean_abs_grad)
       
     # return grad / (self.running_scales.unsqueeze(0) + 1e-12)
   
@@ -178,7 +177,7 @@ class Trainer:
 
       with torch.enable_grad():
         step = self.optimizer_mlp(inputs)
-        step = split_tensorclass(gaussians, step)**2  * grad
+        step = split_tensorclass(gaussians, step)
 
         loss = self.render_step(gaussians - step)
         print(f'{i:3d}: {loss:.4f}')
@@ -190,25 +189,25 @@ class Trainer:
 
 
 
-def mlp(inputs, outputs, hidden_channels: List[int], activation=nn.ReLU, output_activation=None):
+def mlp(inputs, outputs, hidden_channels: List[int], activation=nn.ReLU, norm=nn.Identity, output_activation=None):
   def linear(in_features, out_features, init_std=None):
-    m = nn.Linear(in_features, out_features)
+    m = nn.Linear(in_features, out_features, bias=True)
 
     if init_std is not None:
       m.weight.data.normal_(0, init_std)
-      m.bias.data.zero_()
-    # else:
-    #   # m.weight.data.zero_()
-    #   m.bias.data.zero_()
 
+    m.bias.data.zero_()
     return m
 
   return nn.Sequential(
     
-    linear(inputs, hidden_channels[0]), activation(),
-    *[nn.Sequential(linear(hidden_channels[i], hidden_channels[i+1]), activation(), nn.LayerNorm(hidden_channels[i+1]))
+    linear(inputs, hidden_channels[0]), 
+    activation(),
+    norm(hidden_channels[0]),
+
+    *[nn.Sequential(linear(hidden_channels[i], hidden_channels[i+1]), activation(), norm(hidden_channels[i+1]))
       for i in range(len(hidden_channels) - 1)],
-    linear(hidden_channels[-1], outputs, init_std=0.0001),
+    linear(hidden_channels[-1], outputs, init_std=1e-10),
 
     output_activation() if output_activation is not None else nn.Identity()
   )   
@@ -250,13 +249,13 @@ def main():
   hidden_channels = [256, 256, 256, channels]  # Hidden layers 
   point_optimizer_mlp = mlp(inputs = channels, outputs=channels, 
               hidden_channels=hidden_channels, 
-              activation=nn.ReLU)
+              activation=nn.ReLU,
+              norm=nn.LayerNorm
+              )
   point_optimizer_mlp.to(device=device)
 
   point_optimizer_mlp = torch.compile(point_optimizer_mlp)
-  
-  mlp_opt = torch.optim.AdamW(point_optimizer_mlp.parameters(), lr=0.0001)
-
+  mlp_opt = torch.optim.Adam(point_optimizer_mlp.parameters(), lr=0.0001)
 
   ref_image = torch.from_numpy(ref_image).to(dtype=torch.float32, device=device) / 255 
   config = RasterConfig()
@@ -273,7 +272,7 @@ def main():
     metrics = {}
 
     # Set warmup schedule for first iterations - log interpolate 
-    step_size = log_lerp(min(iteration / 1000., 1.0), 0.001, 1.0)
+    step_size = log_lerp(min(iteration / 1000., 1.0), 1.0, 1.0)
     
     gaussians = trainer.train_epoch(gaussians, epoch_size=epoch_size, step_size=step_size)
 
