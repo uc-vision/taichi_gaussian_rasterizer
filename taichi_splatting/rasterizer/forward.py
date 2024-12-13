dbe20ceb5fa1cb9899da2169947f3669346bab49
Oliver Batchelor <saulzar@gmail.com>
2024-12-14 00:32:15 +1300

    .

--- taichi_splatting/rasterizer/forward.py
+++ taichi_splatting/rasterizer/forward.py
@@ -1,3 +1,4 @@
+from dataclasses import replace
 from functools import cache
 import taichi as ti
 from taichi_splatting.data_types import RasterConfig
@@ -18,25 +19,12 @@ def forward_kernel(config: RasterConfig, feature_size: int, dtype=ti.f32):
     tile_size = config.tile_size
     tile_area = tile_size * tile_size
 
-    # Match backward.py pattern for pixel tiling
-    thread_pixels = config.pixel_stride[0] * config.pixel_stride[1]
-    block_area = tile_area // thread_pixels
-
-    # Create batch types for accumulation
-    thread_features = ti.types.matrix(thread_pixels, feature_size, dtype=dtype)
-    thread_u32 = ti.types.vector(thread_pixels, dtype=ti.u32)
-    thread_i32 = ti.types.vector(thread_pixels, dtype=ti.i32)
-
-    thread_hits = ti.types.matrix(thread_pixels, config.samples, dtype=ti.u32)
     hit_vector = ti.types.vector(config.samples, dtype=ti.u32)
-
-    # Create pixel tile mapping
-    pixel_tile = tuple([ (i, 
-                (i % config.pixel_stride[0],
-                i // config.pixel_stride[0]))
-                  for i in range(thread_pixels) ])
-
     gaussian_pdf = lib.gaussian_pdf_antialias if config.antialias else lib.gaussian_pdf
+
+    @ti.func
+    def round_up(a, b):
+        return (a + (b - 1)) // b
 
     @ti.kernel
     def _forward_kernel(
@@ -53,40 +41,36 @@ def forward_kernel(config: RasterConfig, feature_size: int, dtype=ti.f32):
         tiles_wide = (camera_width + tile_size - 1) // tile_size 
         tiles_high = (camera_height + tile_size - 1) // tile_size
 
-        ti.loop_config(block_dim=(block_area))
-        for tile_id, tile_idx in ti.ndrange(tiles_wide * tiles_high, block_area):
-            pixel_base = tiling.tile_transform(tile_id, tile_idx, 
-                            tile_size, config.pixel_stride, tiles_wide)
+        ti.loop_config(block_dim=(tile_area))
+        for tile_id, tile_idx in ti.ndrange(tiles_wide * tiles_high, tile_area):
+            pixel = tiling.tile_transform(tile_id, tile_idx, 
+                            tile_size, (1, 1), tiles_wide)
+            pixelf = ti.cast(pixel, dtype) + 0.5
 
             # Initialize accumulators for all pixels in tile
-            accum_features = thread_features(0.0)
-            remaining_samples = thread_i32(config.samples)
-            rng_states = thread_u32(0)
-            hit_index = thread_i32(0)
-            hits = thread_hits(0)
+            in_bounds = pixel.y < camera_height and pixel.x < camera_width
 
-            # Initialize RNG states and check bounds for all pixels in tile
-            for i, offset in ti.static(pixel_tile):
-                pixel = pixel_base + ti.Vector(offset)
-                in_bounds = pixel.y < camera_height and pixel.x < camera_width
-                rng_states[i] = wang_hash(ti.u32(pixel.x), ti.u32(pixel.y), seed)
-                remaining_samples[i] = config.samples * ti.i32(in_bounds)
+            accum_features = feature_vec(0.0)
+            remaining_samples = ti.i32(config.samples) * ti.i32(in_bounds)
+            hit_index = ti.i32(0)
+            hits = hit_vector(0)
+
+            rng_states = wang_hash(ti.u32(pixel.x), ti.u32(pixel.y), seed)
 
             start_offset, end_offset = tile_overlap_ranges[tile_id]
             tile_point_count = end_offset - start_offset
-            num_point_groups = (tile_point_count + ti.static(block_area - 1)) // block_area
+            num_point_groups = round_up(tile_point_count, tile_area)
 
             # open shared memory
-            tile_point = ti.simt.block.SharedArray((block_area, ), dtype=Gaussian2D.vec)
-            tile_feature = ti.simt.block.SharedArray((block_area, ), dtype=feature_vec)
+            tile_point = ti.simt.block.SharedArray((tile_area, ), dtype=Gaussian2D.vec)
+            tile_feature = ti.simt.block.SharedArray((tile_area, ), dtype=feature_vec)
 
             for point_group_id in range(num_point_groups):
-                remaining = remaining_samples.sum()
-                if not ti.simt.block.sync_any_nonzero(remaining):
+                if not ti.simt.block.sync_any_nonzero(remaining_samples):
                     break
 
                 # Load points into shared memory
-                group_start_offset = start_offset + point_group_id * block_area
+                group_start_offset = start_offset + point_group_id * tile_area
                 load_index = group_start_offset + tile_idx
 
                 if load_index < end_offset:
@@ -96,44 +80,41 @@ def forward_kernel(config: RasterConfig, feature_size: int, dtype=ti.f32):
 
                 ti.simt.block.sync()
 
-                max_point_group_offset = ti.min(
-                    block_area, tile_point_count - point_group_id * block_area)
+                remaining_points = tile_point_count - point_group_id
 
                 # Process all points in group for each pixel in tile
-                for in_group_idx in range(max_point_group_offset):
+                for in_group_idx in range(min(tile_area, remaining_points)):
+                    if remaining_samples == 0:
+                      break
+
                     mean, axis, sigma, point_alpha = Gaussian2D.unpack(tile_point[in_group_idx])
 
-                    for i, offset in ti.static(pixel_tile):
-                        if remaining_samples[i] > 0:
-                            pixel = pixel_base + ti.Vector(offset)
-                            pixelf = ti.cast(pixel, dtype) + 0.5
+                    gaussian_alpha = gaussian_pdf(pixelf, mean, axis, sigma)
+                    alpha = point_alpha * gaussian_alpha
+                    alpha = ti.min(alpha, ti.static(config.clamp_max_alpha))
 
-                            gaussian_alpha = gaussian_pdf(pixelf, mean, axis, sigma)
-                            alpha = point_alpha * gaussian_alpha
-                            alpha = ti.min(alpha, ti.static(config.clamp_max_alpha))
+                    u, rng_states = xoshiro128(rng_states)
+                    new_hits = min(remaining_samples, 
+                                  bernoulli(u, alpha, config.samples))
+                    
+                    if new_hits > 0:
+                        accum_features += (
+                            tile_feature[in_group_idx] * new_hits / config.samples
+                        )
 
-                            u, rng_states[i] = xoshiro128(rng_states[i])
-                            new_hits = min(remaining_samples[i], 
-                                         bernoulli(u, alpha, config.samples))
-                            
-                            if new_hits > 0:
-                                accum_features[i, :] += (
-                                    tile_feature[in_group_idx] * new_hits / config.samples
-                                )
-                                index =  group_start_offset + in_group_idx + 1
-                                encoded = ti.u32(index) << ti.u32(6) | ti.u32(new_hits)
-                                hits[i, hit_index[i]] = encoded
-                                hit_index[i] += 1
 
-                                remaining_samples[i] -= new_hits
+                        index =  group_start_offset + in_group_idx + 1
+                        encoded = ti.u32(index) << ti.u32(6) | ti.u32(new_hits)
+                        hits[hit_index] = encoded
+                        hit_index += 1
+
+                        remaining_samples -= new_hits
 
 
             # Write final results
-            for i, offset in ti.static(pixel_tile):
-                pixel = pixel_base + ti.Vector(offset)
-                if pixel.y < camera_height and pixel.x < camera_width:
-                    image_feature[pixel.y, pixel.x] = accum_features[i, :]
-                    image_hits[pixel.y, pixel.x] = hits[i, :]
+            if pixel.y < camera_height and pixel.x < camera_width:
+                image_feature[pixel.y, pixel.x] = accum_features
+                image_hits[pixel.y, pixel.x] = hits
     return _forward_kernel
 
 
